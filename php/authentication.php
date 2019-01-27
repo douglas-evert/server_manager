8691199e5d9ddfccb898f96c37dc8bb9b5eded66
Douglas Evert <douglas2913@gmail.com>
2019-01-26 22:42:38 -0500

    update repo

---
+++ php/authentication.php
@@ -0,0 +1,10 @@
+<?php
+//declare global variables
+$username = $_POST[user_username]
+$password = $_POST[user_password] 
+
+function authenticate_connection(){
+    connection = new database_connection("authentication");
+    
+}
+?>
