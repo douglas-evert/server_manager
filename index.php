c0596025d2aa6d0542ea776f8819568d03f40aa4
Douglas Evert <douglas.evert@outlook.com>
2019-02-18 08:05:17 -0500

    Framing out the page on index.php

--- index.php
+++ index.php
@@ -5,6 +5,18 @@
     <link rel="stylesheet" type="text/css" media="screen" href="./style/main.css" />
 </head>
 <body>
+    <div class="frame100">
+        <div class="title_bar">
+            <p> Title </p> 
+        </div>
+        <div class="left_side_bar">
+            <p> left side bar</p>
+        </div>
+        <div class="main_body">
+            <p> Main Body </p>
+        </div>
+
+    </div>
     <?php
     include './php/header.php'
     ?>
--- php/authentication.php
+++ php/authentication.php
@@ -1,7 +1,7 @@
 <?php
 //declare global variables
-$username = $_POST[user_username]
-$password = $_POST[user_password] 
+$username = $_POST[user_username];
+$password = $_POST[user_password]; 
 
 function authenticate_connection(){
     connection = new database_connection("authentication");
